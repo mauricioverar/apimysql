45fdc6c8173300aec8b324042c32d7e9e6fabe95
Mauriciovr7 <52289594+Zentella@users.noreply.github.com>
2022-09-14 19:59:43 -0400

    connection ok

--- server.js
+++ server.js
@@ -1,19 +1,56 @@
-const express = require('express')
-
+const express = require('express') // body parser lo trae incluido o npm i body-parser
+const mysql = require('mysql2/promise') //const mysql = require('mysql2/promise')
 const app = express()
 
 const port = process.env.PORT || 3000 // asignar un puerto en heroku o usar 3000
 
-// middlewares
+// middlewares se ejecutan antes de las funcionalidades y rutas
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
 
-// Routes
-app.use(require('./routes/routes'))
+// create the connection to database
 
+async function main() {
 
-app.listen(port, () => {
-  console.log(`Servidor en puerto http://localhost:${port}`)
-})
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456789',
+    database: 'company'
+  })
+  const res = await connection.query( 'select * from employees' )
+  console.log(res[0])
+  // execute will internally call prepare and query
+  /* connection.execute(
+    'SELECT * FROM employees',
+    function(err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      //console.log(fields); // fields contains extra meta data about results, if available
+  
+      // If you execute same statement again, it will be picked from a LRU cache
+      // which will save query preparation time and give better performance
+    }
+  ) */
+}
+main()
 
-// nodemon server
+/* 
+async function main() {
+
+  const connection = await createPool({
+    host     : 'localhost',
+    user     : 'root',
+    password: '123456789',
+    database : 'company'
+  })
+  connection.connect(function (err) {
+    if(err) {
+      console.log(err);
+      return
+    } else {
+      console.log('db conectada');
+    }})
+}
+main() */
+  
+
+app.listen(port, () => console.log(`server runing on port ${port}`))
---
+++ server2.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+
+const app = express()
+
+const port = process.env.PORT || 3000 // asignar un puerto en heroku o usar 3000
+
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// Routes
+app.use(require('./routes/routes'))
+
+
+app.listen(port, () => {
+  console.log(`Servidor en puerto http://localhost:${port}`)
+})
+
+// nodemon server
