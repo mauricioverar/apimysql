7b953da2d4d7181429b4e735a66c748572a77c01
Mauriciovr7 <52289594+Zentella@users.noreply.github.com>
2022-09-15 17:02:10 -0400

    db planet vercel

--- database.js
+++ database.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
 
 const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456789',
-  database: 'company',
+  host: 'us-east.connect.psdb.cloud',
+  user: 'l198bm38faafjwu4xiod',
+  password: 'pscale_pw_L53RHjlak5bz5fWnkqtwPHebUsFsp7ta1Kjsyn58WzP',
+  database: 'apimysql',
   multipleStatements: true
 });
+
+// DATABASE_URL='mysql://l198bm38faafjwu4xiod:pscale_pw_L53RHjlak5bz5fWnkqtwPHebUsFsp7ta1Kjsyn58WzP@us-east.connect.psdb.cloud/apimysql?ssl={"rejectUnauthorized":true}'
 
 mysqlConnection.connect(function (err) {
   if (err) {
