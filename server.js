bede73649284178004f563601cd6065d72c2dc0f
Mauriciovr7 <52289594+Zentella@users.noreply.github.com>
2022-09-15 10:13:26 -0400

    rutas

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 3000 // asignar un puerto en heroku o usar 3000
 app.use(express.json())
 
 // create the connection to database
-
 async function main() {
 
   const connection = await mysql.createConnection({
@@ -19,38 +18,32 @@ async function main() {
   })
   const res = await connection.query( 'select * from employees' )
   console.log(res[0])
-  // execute will internally call prepare and query
-  /* connection.execute(
-    'SELECT * FROM employees',
-    function(err, results, fields) {
-      console.log(results); // results contains rows returned by server
-      //console.log(fields); // fields contains extra meta data about results, if available
-  
-      // If you execute same statement again, it will be picked from a LRU cache
-      // which will save query preparation time and give better performance
-    }
-  ) */
 }
 main()
-
-/* 
-async function main() {
-
-  const connection = await createPool({
-    host     : 'localhost',
-    user     : 'root',
-    password: '123456789',
-    database : 'company'
-  })
-  connection.connect(function (err) {
-    if(err) {
-      console.log(err);
-      return
-    } else {
-      console.log('db conectada');
-    }})
-}
-main() */
   
+// routes
+app.get('/', (req, res) => {
+  res.send('Api')
+})
 
+// rutes with endpoints
+app.get('/employees', (req, res) => {
+  res.send('List of employees')
+})
+
+app.get('/employee/:id', (req, res) => {
+  res.send('Get employee by id')
+})
+
+app.post('/add', (req, res) => {
+  res.send('New employee')
+})
+
+app.put('/update/:id', (req, res) => {
+  res.send('Update employee')
+})
+
+app.delete('/delete/:id', (req, res) => {
+  res.send('Delete employee')
+})
 app.listen(port, () => console.log(`server runing on port ${port}`))
